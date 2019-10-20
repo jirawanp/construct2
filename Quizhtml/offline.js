8c3c7a4e00d9b35602994d4b2478e1df0e606e8f
jirawanp <56786054+jirawanp@users.noreply.github.com>
2019-10-20 15:59:29 +0700

    upload game
    first

---
+++ Quizhtml/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1571561385,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/answerim1-sheet0.png",
+		"images/answerim2-sheet0.png",
+		"images/answerim3-sheet0.png",
+		"images/answerim4-sheet0.png",
+		"images/block1-sheet0.png",
+		"images/player1-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/play-sheet0.png",
+		"images/play1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"qq.json"
+	]
+}
